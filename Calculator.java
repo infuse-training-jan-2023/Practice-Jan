c6a78716a00b85cb8c547ce6f7d2ede11f5fb655
Minaxi Patil <minaxi.patil@infuse.it>
2023-01-18 23:02:19 +0530

    added calculator file

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class Calculator {
+	private int firstNumber;
+	private int secondNumber;
+	
+	public int addition() {
+		return firstNumber + secondNumber;
+	}
+	
+	
+
+}
